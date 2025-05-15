1953b63d62587a0db42bfcd4e4228e715761b6a3
naingkokolin <naingkokolin004@gmail.com>
2025-05-15 22:53:20 +0630

    structure the project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const carsData = require('./data/cars.json');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/api/cars', (req, res) => {
+  res.json(carsData);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
